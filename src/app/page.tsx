359b375feeff54c3e5b428ffa2d617e92922b6c7
vantu2801se <nguyenvantu28011998it@gmail.com>
2025-02-06 18:27:17 +0700

    init: create Tu Nguyen profile repository

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen text-neutral-900 text-4xl flex items-center justify-center select-none">
+      Tu Nguyen
+    </div>
+  );
+}
