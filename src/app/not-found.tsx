c0df5a6446d603c4dc16ff3ce6f9b6784e65c28f
vantu2801se <nguyenvantu28011998it@gmail.com>
2025-03-27 01:43:20 +0700

    test not-found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+      <div>
+        <h1>Page Not Found</h1>
+        <p>Oops! The page you are looking for does not exist.</p>
+      </div>
+    );
+}
