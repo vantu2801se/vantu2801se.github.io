1ce42c75e364d3dae6ba13c9bb3df7f09165b65d
vantu2801se <nguyenvantu28011998it@gmail.com>
2025-03-27 07:48:46 +0700

    fix: add generateStaticParams

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,7 +3,9 @@ import Experience from "@/components/experience";
 import Footer from "@/components/footer";
 import Introduction from "@/components/introduction";
 import KeyCapabilities from "@/components/keyCapabilities";
-
+export async function generateStaticParams() {
+  return [{ locale: "en" }, { locale: "ja" }, { locale: "vi" }]; 
+}
 export default function Home() {
   return (
     <div className="min-h-screen text-neutral-800 text-4xl select-none">
