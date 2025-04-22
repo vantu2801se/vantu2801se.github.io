933e7a76304b7af0fa32ef96e93939ca2db338ed
vantu2801se <nguyenvantu28011998it@gmail.com>
2025-04-22 14:16:46 +0700

    update download

--- src/locales/devops.ts
+++ src/locales/devops.ts
@@ -1,7 +1,7 @@
 const ENLISH = {
     "download_cv":"Download",
     "download_location":"/pdf/tu-nguyen-en.pdf",
-    "download_file_name": "tu-nguyen-en.pdf",
+    "download_file_name": "tu-nguyen-devops.pdf",
     "born":"01-28-1998",
 
     "introduction_header_name": "TU NGUYEN",
