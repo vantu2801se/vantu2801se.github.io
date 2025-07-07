2869e2ef47e2372fb10bd093aabcb276041be846
nguyenvantu28011998 <nguyenvantu28011998it@gmail.com>
2025-07-07 13:26:44 +0700

    chore: update cv (vi)

--- src/components/se-experience.tsx
+++ src/components/se-experience.tsx
@@ -3,16 +3,35 @@ import useTranslate from "@/hooks/useTranslate";
 
 export default function SoftwareExperience(){
     const {t} = useTranslate();
-    const keys = [t("experience_timeline_1_do_1"),t("experience_timeline_1_do_2"),t("experience_timeline_1_do_3"), t("experience_timeline_1_do_4"), t("experience_timeline_1_do_5"), t("experience_timeline_1_do_6")];
+    const keys1 = [t("experience_timeline_1_do_1"),t("experience_timeline_1_do_2"),t("experience_timeline_1_do_3")];
+    const keys2 = [t("experience_timeline_2_do_1"),t("experience_timeline_2_do_2"),t("experience_timeline_2_do_3"),t("experience_timeline_2_do_4"),t("experience_timeline_2_do_5"),t("experience_timeline_2_do_6")];
+    const keys3 = [t("experience_timeline_3_do_1"),t("experience_timeline_3_do_2")];
 
-    return <div className="flex flex-col select-text">
+    return <div className="flex flex-col select-text w-[800px]">
         <h3 className="text-xl font-bold text-left">{t("experience_title")}</h3>
         <h4 className="text-lg font-bold text-left pl-2">{t("experience_timeline_1")}</h4>
         
-        <ul className="list-disc pl-10 break-words w-[800px]">
-            {keys.map((item, index) => (
+        <ul className="list-disc pl-10 break-words w-[max-w-full]">
+            {keys1.map((item, index) => (
                 <li key={index} className="text-lg">{item}</li>
         ))}
         </ul>
+
+        <h4 className="text-lg font-bold text-left pl-2">{t("experience_timeline_2")}</h4>
+        <ul className="list-disc pl-10 break-words w-[max-w-full]">
+            {keys2.map((item, index) => (
+                <li key={index} className="text-lg">{item}</li>
+        ))}
+        </ul>
+
+        <h4 className="text-lg font-bold text-left pl-2">{t("experience_timeline_3")}</h4>
+        <ul className="list-disc pl-10 break-words w-[max-w-full]">
+            {keys3.map((item, index) => (
+                <li key={index} className="text-lg">{item}</li>
+        ))}
+        </ul>
+
+        <h4 className="text-lg font-bold text-left pl-2">{t("experience_timeline_4")}</h4>
+        
     </div>
 }
